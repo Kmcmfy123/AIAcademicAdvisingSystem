7652363c873a1679fd808329b3ed4aabfa0488db
Jelo <adrianmvalmonte@gmail.com>
2025-12-12 02:37:52 +0800

    upd

--- configMail.php
+++ configMail.php
@@ -2,7 +2,7 @@
 return [
     'host'       => 'smtp.gmail.com',
     'username'   => 'amvgdrive2025@gmail.com',       // NO spaces
-    'password'   => 'ptlqtv9h8t622xq6ykngx7bda', // 16-char App Password
+    'password'   => '', // add your 16-char App Password
     'port'       => 587,
     'encryption' => 'ssl',
     'from_email' => 'amvgdrive2025@gmail.com',        // must match username
